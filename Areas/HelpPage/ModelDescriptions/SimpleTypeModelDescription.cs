9aae5b3598004f9fbc532882be0d9ab06ec7f39d
imran zaman <imran_ujhani@yahoo.com>
2020-02-16 13:55:41 -0500

    commit staged

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace duplicate.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace duplicate.Controllers
+{
+    [Authorize]
+    public class EmployeeController : ApiController
+    {
+        public static IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
